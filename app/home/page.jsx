7906a25eb27c7eb349605a2aa7f798fb8dd67014
Andres <142335765+andreUmag@users.noreply.github.com>
2025-03-01 14:21:01 -0500

    home-user-add

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,18 +1,31 @@
-import './home.css';
+import "./home.css";
+import ShinyText from "@/components/ShinyText";
 
 function HomePage() {
   return (
     <div className="home-page">
-      <div className="userteams">
-        <div className="user"></div>
-        <div className="team"></div>
-      </div>
-      <div className="battles">
-
+      <div className="columns">
+        <div className="left-column">
+          <div className="user">
+            <img src="./avatar1.webp" alt="avatar" />
+            <div className="usertags">
+              <ShinyText
+                text="TRAINER"
+                disabled={false}
+                speed={5}
+                className="trainer"
+              />
+              <p className="trainerName">HITMAN RONALD</p>
+            </div>
+          </div>
+          <div className="battles"></div>
+        </div>
+        <div className="right-column">
+          <div className="team"></div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
-
 
 export default HomePage;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -18,7 +18,7 @@ export default function Navbar() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            className="inline-block h-5 w-5 stroke-current"
+            className="inline-block h-8 w-18 stroke-current"
           >
             {" "}
             <path
