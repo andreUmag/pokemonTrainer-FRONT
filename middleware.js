619629785650e4d8c75cc10225429fb2d1b311e4
DavuKX <duvandavid2001@gmail.com>
2025-03-12 19:58:10 -0500

    fix: fix midleware redirect

--- middleware.js
+++ middleware.js
@@ -4,7 +4,7 @@ import {cookies} from "next/headers";
 export async function middleware(request) {
     const userId = (await cookies()).get('user_id');
 
-    if (!userId) {
+    if (!userId && request.url !== '/auth/register') {
         return NextResponse.redirect(new URL('/auth/login', request.url))
     }
 }
